29e4221092e592dee8e64c9c7c5e21b1a5f05c69
Asia <asiaprincigalli@MacBook-Air-di-Asia.local>
2026-06-29 09:06:53 +0200

    Finito punto 1 Fatto modifiche punto 1 numero archi mi viene sbagliato --> possibile errore lettura consegna nonostante modificato come richiesto non viene attesa che pubblichino correzione per punto 1 ricorsione fatta e viene yeeee corretto dao adesso num archi corretto problema con led zeppin ma vabbè

--- tst funzioni.py
+++ tst funzioni.py
@@ -3,3 +3,5 @@ from UI.controller import Controller
 mycontroller = Controller()
 lista_generi = mycontroller.fillDDGenre()
 print(lista_generi)
+#./.venv/bin/python -m pip install mysql-connector-python
+#pip install -r requirements.txt
